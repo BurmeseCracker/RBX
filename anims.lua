b749c1185ed26a034749e1bb45f5f99d17e5d985
Bhone Thit Htoo <112844311+BurmeseCracker@users.noreply.github.com>
2026-06-02 19:33:43 +0900

    Add animation assets for Adidas Aura and Cartoony Pack

---
+++ anims.lua
@@ -0,0 +1,19 @@
+return {
+    ["Adidas Aura"] = {
+        idle =  "rbxassetid://73137983344853",
+        climb = "rbxassetid://140398319728398",
+        fall =  "rbxassetid://99457463463495",
+        jump =  "rbxassetid://129527230938281",
+        run =   "rbxassetid://123973978164540",
+        walk =  "rbxassetid://75183215343859",
+        swim =  "rbxassetid://119007025452432"
+    },
+    ["Cartoony Pack"] = {
+        idle =  "rbxassetid://837011741",
+        walk =  "rbxassetid://837010234",
+        run =   "rbxassetid://837009922",
+        jump =  "rbxassetid://837011171",
+        fall =  "rbxassetid://837010685",
+        climb = "rbxassetid://837013990"
+    }
+}
