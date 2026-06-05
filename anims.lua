a337067752331da220c276fd81c0effc61b03006
Bhone Thit Htoo <112844311+BurmeseCracker@users.noreply.github.com>
2026-06-05 13:31:14 +0900

    Fix missing commas and add new animations

--- anims.lua
+++ anims.lua
@@ -1,15 +1,17 @@
 -- anims.lua
 return {
-    ["AdidasAura"] = {
-        
-        idle =  "rbxassetid://126354114956642",
-        climb = "rbxassetid://140398319728398",
-        fall =  "rbxassetid://99457463463495",
-        jump =  "rbxassetid://129527230938281",
-        run =   "rbxassetid://123973978164540",
-        walk =  "rbxassetid://75183215343859",
-        swim =  "rbxassetid://119007025452432"
-    }, -- added missing comma here
-    ["GrayscaledIdle"] = "rbxassetid://73137983344853", -- added missing comma here
-    ["CuteSit"] = "rbxassetid://71020762961324" -- added missing slash here
+	["AdidasAura"] = {
+		idle =  "rbxassetid://126354114956642",
+		climb = "rbxassetid://140398319728398",
+		fall =  "rbxassetid://99457463463495",
+		jump =  "rbxassetid://129527230938281",
+		run =   "rbxassetid://123973978164540",
+		walk =  "rbxassetid://75183215343859",
+		swim =  "rbxassetid://119007025452432"
+	},
+	["GrayscaledIdle"] = "rbxassetid://73137983344853",
+	["CuteSit"] = "rbxassetid://71020762961324",
+	["CutePose"] = "rbxassetid://96478728586362",
+	["CuteFeetKick"] = "rbxassetid://104104787736608",
+	["ZEN"] = "rbxassetid://82524388758029"
 }
