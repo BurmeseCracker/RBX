ce6d5054224915d4fd612cd0890640a5672aea25
Bhone Thit Htoo <112844311+BurmeseCracker@users.noreply.github.com>
2026-06-08 13:50:04 +0900

    Add new animations to anims.lua

--- anims.lua
+++ anims.lua
@@ -10,8 +10,18 @@ return {
 		swim =  "rbxassetid://119007025452432"
 	},
 	["GrayscaledIdle"] = "rbxassetid://73137983344853",
-	["CuteSit"] = "rbxassetid://71020762961324",
-	["CutePose"] = "rbxassetid://96478728586362",
-	["CuteFeetKick"] = "rbxassetid://104104787736608",
-	["ZEN"] = "rbxassetid://82524388758029"
+	
+	["ZEN"] = "rbxassetid://82524388758029",
+	["KingOfRBX"] = "rbxassetid://96282499056792",
+    ["Sit1"] = "rbxassetid://96606489351695",
+    ["Dance1"] = "rbxassetid://107281732681902",
+	["Punching"] = "rbxassetid://76710893141393",
+	["Headless"] = "rbxassetid://80044657810108",
+	["Chill1"] = "rbxassetid://110571622513305",
+	["LoveU"] = "rbxassetid://122623026395755",
+	["SitCry"] = "rbxassetid://124300863605270",
+	["Haha"] = "rbxassetid://4102315500",
+	["Sad"] = "rbxassetid://4849502101",
+
+	
 }
