c9c477b4ebecbcb100a2bf6e330109f54207f7b5
Bhone Thit Htoo <112844311+BurmeseCracker@users.noreply.github.com>
2026-06-09 17:47:56 +0900

    Reorganize and add dance animations in anims.lua

--- anims.lua
+++ anims.lua
@@ -14,17 +14,30 @@ return {
 	["ZEN"] = "rbxassetid://82524388758029",
 	["KingOfRBX"] = "rbxassetid://96282499056792",
     ["Sit1"] = "rbxassetid://96606489351695",
-    ["Dance1"] = "rbxassetid://107281732681902",
+    
 	["Punching"] = "rbxassetid://76710893141393",
 	["Headless"] = "rbxassetid://80044657810108",
 	["Chill1"] = "rbxassetid://110571622513305",
-	["Love1"] = "rbxassetid://84396003438766",
+
 	["SitCry"] = "rbxassetid://124300863605270",
 	["Haha"] = "rbxassetid://4102315500",
 	["Sad"] = "rbxassetid://4849502101",
 	["Aura"] = "rbxassetid://72752639330978",
 	["HeadPat"] = "rbxassetid://73930469890558",
+	
+	["Love1"] = "rbxassetid://84396003438766",
 	["Love2"] = "rbxassetid://94601583610207",
+	
+	["Dance1"] = "rbxassetid://107281732681902",
+	["Dance2"] = "rbxassetid://3696757129",
+	["Dance3"] = "rbxassetid://109989862061153",
+	["Dance4"] = "rbxassetid://100284504525496",
+	["Dance5"] = "rbxassetid://122286388968004",
+	["Dance6"] = "rbxassetid://136600490799561",
+	["Dance7"] = "rbxassetid://117354698957876"
+	
+
+	
 	
 	
 	
